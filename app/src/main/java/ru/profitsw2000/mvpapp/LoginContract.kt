f2ed7eae694b3d8425b8d827b7ad0ebbb696ec91
profitsw2000 <profitsw2000@gmail.com>
2022-04-04 14:03:53 +0300

    Создание интерфейсов для Model, View и Presenter.

---
+++ app/src/main/java/ru/profitsw2000/mvpapp/LoginContract.kt
@@ -0,0 +1,27 @@
+package ru.profitsw2000.mvpapp
+
+import androidx.annotation.MainThread
+
+class LoginContract {
+
+    interface View {
+        @MainThread
+        fun setSuccess()
+        @MainThread
+        fun setError(error: String)
+        @MainThread
+        fun showProgress()
+        @MainThread
+        fun hideProgress()
+    }
+
+    interface Presenter {
+        fun onAttach(view: View)
+        fun onLogin(login: String, password: String)
+    }
+
+    interface Model {
+        fun getCredentials(): Boolean
+        fun getAccount(): String
+    }
+}
