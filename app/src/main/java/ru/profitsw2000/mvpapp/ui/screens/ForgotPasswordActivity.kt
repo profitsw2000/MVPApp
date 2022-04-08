a78b4c7c89c3307617b0bda693e666d8609c7b32
profitsw2000 <profitsw2000@gmail.com>
2022-04-08 14:24:41 +0300

    Добавлено отдельное активити для восстановления пароля, свёрстан его макет.

---
+++ app/src/main/java/ru/profitsw2000/mvpapp/ui/screens/ForgotPasswordActivity.kt
@@ -0,0 +1,12 @@
+package ru.profitsw2000.mvpapp.ui.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.profitsw2000.mvpapp.R
+
+class ForgotPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
