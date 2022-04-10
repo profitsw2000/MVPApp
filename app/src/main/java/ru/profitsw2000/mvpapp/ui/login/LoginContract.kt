e2cbb957b4132c3f36e71716cf0e91a352d09ded
profitsw2000 <profitsw2000@gmail.com>
2022-04-10 10:01:38 +0300

    Переход с архитектуры MVP на MVVM - создан интерфейс ViewModel и класс LoginViewModel.kt на его основе.

--- app/src/main/java/ru/profitsw2000/mvpapp/ui/login/LoginContract.kt
+++ app/src/main/java/ru/profitsw2000/mvpapp/ui/login/LoginContract.kt
@@ -1,6 +1,7 @@
 package ru.profitsw2000.mvpapp.ui.login
 
 import androidx.annotation.MainThread
+import ru.profitsw2000.mvpapp.utils.Publisher
 
 class LoginContract {
 
@@ -23,6 +24,23 @@ class LoginContract {
         fun onAttach(view: View)
         fun onLogin(login: String, password: String)
         fun onRestorePassword(email: String)
+        fun onSignUp(email: String, login: String, password: String)
+    }
+
+    interface ViewModel {
+        val showProgress: Publisher<Boolean>
+        val isSignInSuccess: Publisher<Boolean>
+        val isRestorePasswordSuccess: Publisher<Boolean>
+        val isSignUpSuccess: Publisher<Boolean>
+        val errorCode: Publisher<Int?>
+
+        @MainThread
+        fun onLogin(login: String, password: String)
+
+        @MainThread
+        fun onRestorePassword(email: String)
+
+        @MainThread
         fun onSignUp(email: String, login: String, password: String)
     }
 }
---
+++ app/src/main/java/ru/profitsw2000/mvpapp/ui/login/LoginViewModel.kt
@@ -0,0 +1,52 @@
+package ru.profitsw2000.mvpapp.ui.login
+
+import ru.profitsw2000.mvpapp.domain.LoginUseCase
+import ru.profitsw2000.mvpapp.utils.Publisher
+
+private const val ERROR_SIGN_IN = 1
+private const val ERROR_PASSWORD_RESTORE = 2
+private const val ERROR_SIGN_UP = 3
+private const val ERROR_EMPTY_FIELD = 4
+
+class LoginViewModel(private val loginUseCase: LoginUseCase): LoginContract.ViewModel {
+    override val showProgress: Publisher<Boolean> = Publisher()
+    override val isSignInSuccess: Publisher<Boolean> = Publisher()
+    override val isRestorePasswordSuccess: Publisher<Boolean> = Publisher()
+    override val isSignUpSuccess: Publisher<Boolean> = Publisher()
+    override val errorCode: Publisher<Int?> = Publisher()
+
+    override fun onLogin(login: String, password: String) {
+        showProgress.post(true)
+        loginUseCase.login(login, password) {result ->
+            if (result) {
+                isSignInSuccess.post(true)
+            } else {
+                errorCode.post(ERROR_SIGN_IN)
+            }
+            showProgress.post(true)
+        }
+    }
+
+    override fun onRestorePassword(email: String) {
+        if (email.isNotEmpty()) {
+            showProgress.post(true)
+            loginUseCase.restorePassword(email) { result ->
+                if (result) isRestorePasswordSuccess.post(true)
+                else errorCode.post(ERROR_PASSWORD_RESTORE)
+                showProgress.post(false)
+            }
+        } else {errorCode.post(ERROR_EMPTY_FIELD)}
+    }
+
+    override fun onSignUp(email: String, login: String, password: String) {
+        if (email.isNotEmpty() && login.isNotEmpty() && password.isNotEmpty()) {
+            showProgress.post(true)
+            loginUseCase.register(login, password, email) { result ->
+                if(result) isSignUpSuccess.post(true)
+                else errorCode.post(ERROR_SIGN_UP)
+                showProgress.post(false)
+            }
+        } else {errorCode.post(ERROR_EMPTY_FIELD)}
+    }
+
+}
