726459f3b6924235fa26b66327bffaa1829be0d7
profitsw2000 <profitsw2000@gmail.com>
2022-08-21 14:20:58 +0300

    Сделаны тесты для EditText и TextView в MainActivity.

--- app/src/androidTest/java/ru/profitsw2000/mvpapp/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/ru/profitsw2000/mvpapp/MainActivityEspressoTest.kt
@@ -1,7 +1,12 @@
 package ru.profitsw2000.mvpapp
 
+import android.widget.TextView
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import junit.framework.TestCase
 import org.junit.After
@@ -30,6 +35,58 @@ class MainActivityEspressoTest {
     @Test
     fun activity_IsResumed() {
         TestCase.assertEquals(Lifecycle.State.RESUMED, scenario.state)
+    }
+
+    @Test
+    fun loginEditText_IsDisplayed() {
+        Espresso.onView(withId(R.id.login_edit_text))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun passwordEditText_IsDisplayed() {
+        Espresso.onView(withId(R.id.password_edit_text))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun forgotPasswordTextView_NotNull() {
+        scenario.onActivity {
+            val forgotPasswordTextView = it.findViewById<TextView>(R.id.tv_forgot_password)
+            TestCase.assertNotNull(forgotPasswordTextView)
+        }
+    }
+
+    @Test
+    fun forgotPasswordTextView_HasText() {
+        val assertion = ViewAssertions.matches(ViewMatchers.withText(R.string.forgot_password_text))
+        Espresso.onView(withId(R.id.tv_forgot_password)).check(assertion)
+    }
+
+    @Test
+    fun forgotPasswordTextView_IsDisplayed() {
+        Espresso.onView(withId(R.id.tv_forgot_password))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun signUpTextView_NotNull() {
+        scenario.onActivity {
+            val signUpTextView = it.findViewById<TextView>(R.id.tv_sign_up)
+            TestCase.assertNotNull(signUpTextView)
+        }
+    }
+
+    @Test
+    fun signUpTextView_HasText() {
+        val assertion = ViewAssertions.matches(ViewMatchers.withText(R.string.sign_up_text))
+        Espresso.onView(withId(R.id.tv_sign_up)).check(assertion)
+    }
+
+    @Test
+    fun signUpTextView_IsDisplayed() {
+        Espresso.onView(withId(R.id.tv_sign_up))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
     @After
